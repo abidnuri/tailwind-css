22d7ca3d95a496c2a24b9014be804d473137678f
Abid Nuri <mdabidnuri@gmail.com>
2021-06-09 19:12:57 +0600

    tailwind website

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import HeadContent from '../HeadContent/HeadContent';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <HeadContent />
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
